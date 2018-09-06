9e0e94414d702886011f530c4f569fe331f7fdc5
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-09-06 18:20:10 +1200

    added getCandidate

--- src/server/api/controllers/candidate.js
+++ src/server/api/controllers/candidate.js
@@ -1,21 +1,44 @@
 "use strict";
 
-const candidates = require("../../data/candidates");
+const candidateHandler = require("../../data/candidates");
+const jobHandler = require("../../data/jobs");
+const applicationHandler = require(".../data/applications");
 
-exports.createCandidate = function (request, response, next) {
+exports.createCandidate = function(request, response, next) {
+  var candidate = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email
+  };
 
-    var candidate = {
-        firstName: request.body.firstName,
-        lastName: request.body.lastName,
-        email: request.body.email
-    }
+  candidateHandler.putCandidate(candidate);
 
-    candidates.putCandidate(candidate);
+  // var getCandidate = candidates.getCandidate("contact@anthonywong.co.nz");
+  // console.log("Found candidates: " + candidate.FirstName + " " + candidate.LastName);
+  console.log(JSON.stringify(candidate, null, 2));
 
-    // var getCandidate = candidates.getCandidate("contact@anthonywong.co.nz");
-    // console.log("Found candidates: " + candidate.FirstName + " " + candidate.LastName);
-    console.log(JSON.stringify(candidate, null, 2));
+  response.statusCode = 200;
+  response.end();
+};
 
-    response.statusCode = 200;
-    response.end();
+exports.getCandidate = function(request, response, next) {
+  let result = {};
+
+  const candidateEmail = request.body;
+  const candidate = candidateHandler.getCandidate(candidateEmail);
+  result.push(candidate);
+  const applications = applicationHandler.getApplication(candidateEmail);
+  result.push(applications);
+
+  for (var application of result.applications) {
+    const job = jobHandler.getJob(application.referenceNumber);
+    application.push(job.jobTitle);
+    application.push(job.jobDescription);
+  }
+
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "application/json; charset=utf-8");
+  response.write(JSON.stringify(result, null, 2));
+
+  response.end();
 };
