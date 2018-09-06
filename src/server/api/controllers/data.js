932c5b99fbfdc7608ab5d9640166adc959e3ba4e
awon150 <wong.anthony10@gmail.com>
2018-09-06 14:49:42 +1200

    Global var test

---
+++ src/server/api/controllers/data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var jobs_data = {
+    "job_description": "SQL Developer",
+    "reference_number": "1",
+    "closing_date": "12/10/2018 00:00:00"
+}
+
+var job_list = [];
--- src/server/api/controllers/helloworld.js
+++ src/server/api/controllers/helloworld.js
@@ -1,20 +1,26 @@
 'use strict';
 
-// let json = '{"job_description":"SQL Developer", "reference_number":42, closing_date:"12/10/2018 00:00:00"}';
 exports.sayHello = function (request, response, next) {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    // console.log(request.body);
-    // console.log(request.body.job_description);
-    let job_data = response.body + ''.split(",");
-    // job_data.append(response.body);
-    console.log(typeof job_data)
-    let test = job_data.split(",");
-    console.log(test);
 
-    response.write("TEST");
+    let json = request.body;
+    // let job_list = [];
+
+    for (var element in json) {
+        job_list.push([element, json[element]]);
+    }
+
+    // console.log(job_list);
+
+    // for (var i = 0; i<job_list.length; i++) {
+    //     console.log(job_list[i][0]);
+    //     console.log(job_list[i][1]);
+    //     console.log("");
+    // }
+
+    // response.write("TEST");
 
     response.end();
-
 
 }
