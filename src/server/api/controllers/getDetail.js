e2fbcf975f593631d8bda546e55199a7977cffa9
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-09-06 15:39:26 +1200

    .

--- src/server/api/controllers/getDetail.js
+++ src/server/api/controllers/getDetail.js
@@ -2,7 +2,6 @@
 
 exports.getDetail = function(request, response, next) {
   const applicantID = request.body.email;
-
   console.log(applications);
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html; charset=utf-8");
