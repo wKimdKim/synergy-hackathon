ad9cdd3aa8a301ee9cdb4158810bf29af7a2a870
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-09-06 15:32:46 +1200

    ?

--- src/server/api/controllers/getDetail.js
+++ src/server/api/controllers/getDetail.js
@@ -1,30 +1,9 @@
 "use strict";
 
-var applications = {
-  jobs: {
-    "SQL Developer": [
-      { email: "wkimdkim@gmail.com" },
-      { email: "abc@gmail.com" },
-      { email: "def@gmail.com" }
-    ],
-    "Front-End Developer": [
-      { email: "wkimdkim@gmail.com" },
-      { email: "abc@gmail.com" },
-      { email: "def@gmail.com" }
-    ],
-    "Back-End Developer": [
-      { email: "wkimdkim@gmail.com" },
-      { email: "abc@gmail.com" },
-      { email: "def@gmail.com" }
-    ]
-  }
-};
-
 exports.getDetail = function(request, response, next) {
   const applicantID = request.body.email;
-  for (const application of applications.jobs) {
-    console.log(application);
-  }
+
+  console.log(applications);
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html; charset=utf-8");
 };
