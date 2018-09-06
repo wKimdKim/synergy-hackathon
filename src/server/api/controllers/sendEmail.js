ac1020b036e9840ea241cb86241142e803c2f8c3
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-09-06 12:59:51 +1200

    Send Email controller

---
+++ src/server/api/controllers/sendEmail.js
@@ -0,0 +1,44 @@
+"use strict";
+const nodemailer = require("nodemailer");
+/*{
+    "name": "abc",
+    "email": "abc@gmail.com",
+    "stage": "abc"
+    }*/
+exports.sendEmail = function(request, response, next) {
+  const email = request.body.email;
+  const name = request.body.name;
+  const stage = request.body.stage;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "SynergyTestEmail123@gmail.com",
+      pass: "AnthonyWong"
+    }
+  });
+
+  const mailOptions = {
+    from: "SynergyTestEmail123@gmail.com",
+    to: email,
+    subject:
+      "Congratulations " +
+      request.body.name +
+      "! You have moved onto the " +
+      request.body.stage +
+      " stage.",
+    text: "hohoho"
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  response.statusCode = 200;
+  // response.setHeader('Content-Type', 'text/html; charset=utf-8');
+  // response.write("Hello World!");
+  response.end();
+};
--- src/server/server.js
+++ src/server/server.js
@@ -1,37 +1,43 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Routing handlers
-const helloWorld = require('./api/controllers/helloworld');
+const helloWorld = require("./api/controllers/helloworld");
+const emailer = require("./api/controllers/sendEmail");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve static HTML as well
-app.use(express.static(path.resolve(__dirname + '/../client')));
+app.use(express.static(path.resolve(__dirname + "/../client")));
 
 // Log all requests
-app.all('*', function (request, response, next) {
-  console.log(request.method + ': ' + request.originalUrl);
+app.all("*", function(request, response, next) {
+  console.log(request.method + ": " + request.originalUrl);
   next(); // pass control to the next handler
 });
 
 // ROUTES
-app.route('/helloworld')
-  .get(function (request, response, next) {
-    return helloWorld.sayHello(request, response, next);
-  });
+app.route("/helloworld").get(function(request, response, next) {
+  return helloWorld.sayHello(request, response, next);
+});
+
+app.route("/sendEmail").put(function(request, response, next) {
+  return emailer.sendEmail(request, response, next);
+});
 
 // Handle errors
 app.use(function(error, request, response, next) {
   console.error(error);
-  response.status(error.status || 500).json({status: error.status, message: error.message})
+  response
+    .status(error.status || 500)
+    .json({ status: error.status, message: error.message });
 });
 
 app.listen(port);
 
-console.log('Server started: ' + port);
+console.log("Server started: " + port);
